37ec2b019f19eb549ee095ded74452df9404e475
Terence Chow <terence821@gmail.com>
2018-12-23 15:58:06 +0800

    cpp_server_cppcms: Added optional "-m" flag for user defined MapGraph file path.                    Omit for default path: same directory with executable.

--- cpp_server_cppcms/src/main.cpp
+++ cpp_server_cppcms/src/main.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include <cppcms/applications_pool.h>
 #include <cppcms/service.h>
 
@@ -6,10 +8,43 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
+/* Helper function to get executable directory w.r.t. current working directory from command */
+string get_current_dir(string exec_name)
 {
-    const string map_file_path = "./HK.graph";
+    int last_slash = exec_name.rfind("/");
+    return exec_name.substr(0, last_slash);
+}
 
+/* Helper function to get MapGraph file path */
+string get_map_file_path(const int & argc, char ** argv) {
+    if(argc > 3)
+    {
+        if(strcmp(argv[3], "-m") == 0)
+        {
+            /* Get user defined file path. */
+            return string(argv[4]);
+        }
+        else
+        {
+            /* Invalid argument, exit */
+            cout << "Usage: " << argv[0] << " -c <config.js> [-m <map file path>]" << endl;
+            exit(1);
+        }
+    }
+    else
+    {
+        /* Use default MapGraph file dir: executable directory */
+        const string map_file_name = "HK.graph";
+        const string map_file_dir = get_current_dir(string(argv[0]));
+        return map_file_dir + "/" + map_file_name;
+    }
+}
+
+/* Driver function of the server */
+int main(int argc, char ** argv)
+{
+    string map_file_path = get_map_file_path(argc, argv);
+    cout << "Using MapGraph file at: " << map_file_path << endl;
     try
     {
         cout << "Initializing MapGraph..." << endl;
@@ -22,5 +57,8 @@ int main(int argc, char **argv)
     catch (std::exception const &e)
     {
         std::cerr << e.what() << std::endl;
+        return 1;
     }
+
+    return 0;
 }
